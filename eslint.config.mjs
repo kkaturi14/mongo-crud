2145bfb4ae4694feca814c0339e7ff88367c3232
kkaturi14 <kkaturi14@gmail.com>
2025-12-10 05:26:50 +0900

    Update Next.js to 16.0.8 and fix ESLint config for Vercel deployment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -22,23 +22,4 @@ const eslintConfig = [
   },
 ];
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-      },
-    ],
-  },
-}
-
-module.exports = nextConfig
-
-
 export default eslintConfig;
